efdc61463b34a09eceadb3ef6ce05d622d2acba3
Anton Brandt Pedersen <antonbrandtpedersen@Antons-MacBook-Air.local>
2018-10-29 12:01:44 +0100

    added styleFiles to productpages

--- js/RecyclableProductPage.js
+++ js/RecyclableProductPage.js
@@ -2,19 +2,20 @@
 //create class "product" - within dropdown bar of size, number of diapers, priceCalc
 
 //create an empty array, because default cart is empty
+
 var cart = [];
 
 //define item to show in cart
-var Item = function(name, size, price, count) {
+var item = function(name, size, price, count) {
     this.name = name
-    this.size = name
+    this.diaperSize = size
     this.price = price
     this.count = count
 };
 
 //Job of the function: create a new item and add it to the cart
 function addItemToCart(name, size, price, count) {
-    var item = new Item(name, size, price, count);
+    var item = new item(name, size, price, count);
     cart.push(item) //-> add new item
 }
 
