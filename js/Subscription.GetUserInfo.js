2f4809dab33df4f239d982c2f57bd1e69ff843d9
Johannes Reisinger <Johannes@Johanness-MacBook-Air.local>
2018-11-17 18:00:02 +0100

    Table

--- js/Subscription.GetUserInfo.js
+++ js/Subscription.GetUserInfo.js
@@ -1,65 +1,60 @@
 var users = JSON.parse(localStorage.getItem("users"));   
 
-function createHTML() {
-    return "<li class='userInfo'><img width='200px' src=" + this.firstname + ">" +
-    "<h1>"+ this.lastname + "</h1>" +
-    // "<p>" + this.description + "</p>" +
-    // "<p>Price: " + this.price + "</p>" +
-    // this.buttonCart +
-    "</li>"
-}
-
-    
-
-//   print() { 
-//         return "<tr><td> <img height='65px' src='" + this.firstname + "'></td><td>" + this.lastname + "</td><td>" + this.username + "</td><td>" + this.email + "</td></tr>";
-//     }
-
-    
-// }
-
-
-
-    //   print() {
-    //     console.log(this.firstname + ' ' + this.lastname + ' ')
-    //   }
-
-
-    function printUserInformation() {
-        var html = ''
-              for(i=0; i<this.users.length; i++){
-                  html += this.users[i].print()
-              }
-          
-          }
-
-
 function displayUserInformation() {
    
-
-    var html = '<tr><td>First Name</td><td>Last Name</td><td>Username</td><td>Password</td></tr>'
-
+   
         for (i=0; i < users.length; i++){
             if(users[i].userId == localStorage.getItem('loggedInUser')){
                
-                // var activeUser = users[i]
-                console.log(users[i]);
+                var activeUser = users[i]
+                showTable(activeUser);
+                console.log(users[i]['lastname']);
                 console.log(localStorage.getItem('loggedInUser'));
-        }
-       
-            // html += activeUser.printUserInformation();
-               
-        }
-
-        // document.getElementById('userInfo').innerHTML 
-        
+            }          
+        }     
     }
+
+function showTable(user) {
+
+    //Create Table reference
+    var dvTable = document.getElementById('userInfo');
+    dvTable.innerHTML = "";
+    
+    //Create Table
+    var table = document.createElement("TABLE");
+    table.border=1;
+
+    //Init Header Row
+    var columns = [];
+    columns.push("Username","Firstname","Lastname","Email");
+    var row = table.insertRow(-1);
+
+    //Fill out Header Row
+    for (var i =0; i<columns.length; i++) {
+        var headerCell = document.createElement("TH");
+        headerCell.innerHTML = columns[i];
+        row.appendChild(headerCell);
+    }
+
+    //User Row
+        row = table.insertRow(-1);
+
+        var cell1 = row.insertCell(-1);
+        cell1.innerHTML = user['username'];
+        var cell2 = row.insertCell(-1);
+        cell2.innerHTML = user['firstname'];
+        var cell3 = row.insertCell(-1);
+        cell3.innerHTML = user['lastname'];
+        var cell4 = row.insertCell(-1);
+        cell4.innerHTML = user['email'];
+       
+    userInfo.appendChild(table);
+}
 
     displayUserInformation();
 
 
-    
 
-    // printUserInformation();
 
     
+
