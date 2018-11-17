e41cf9a4f040e3425ec90310a62316daac8c881d
Anton Brandt Pedersen <antonbrandtpedersen@Antons-MacBook-Air.local>
2018-11-17 10:55:00 +0100

    Added a userId randomizer & index.js

---
+++ js/Index.js
@@ -0,0 +1,14 @@
+
+console.log("hi world"); //it works
+
+var currentUser = localStorage.getItem("loggedInUser");
+console.log(currentUser); // Andy
+
+if (currentUser == 0) {
+    console.log("noUser");
+}
+
+
+// if (localStorage.getItem(LoggedInUser) == ) {
+    
+// // }
