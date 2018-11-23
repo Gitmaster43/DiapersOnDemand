3db86cb8d66cf6b2db39f348d65e99c68dc9fc52
Anton Brandt Pedersen <antonbrandtpedersen@Antons-MacBook-Air.local>
2018-11-23 08:49:02 +0100

    Registration requirements when also CREATING

--- js/RegistrationFunction.js
+++ js/RegistrationFunction.js
@@ -47,13 +47,23 @@ for (var i = 0; i < users.length; i++) {
       if (users[i].username == userName) {
       alert("Username already exists");
       return false;               
-      }}
+      }};
   
+// first () indicates at least 1 special character requirement, second () indicates at least 1 capital letter, third() indicates that the password has to contain at least 6 characters
       
+// Sets password variable equal to user input   
+passwordrequirements ="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"; 
+console.log(password);
+console.log(passwordrequirements.test(password));
+// Checks if password matches criteria of variable pwd
+if ("(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}".test(password)) {
+  return true; 
+} else {
+  alert("password does meet requirments")
+  return false
+};
+
 //loops through the already existing users, and check if the password input already exist. If it does, user is stopped
-
-
-        
 
 
 users.push(new User(firstName, lastName, userName, email, password, phoneNumber, dateOfBirth, streetName, postalNumber, city, userId));
@@ -81,66 +91,66 @@ users.push(new User(firstName, lastName, userName, email, password, phoneNumber,
  
 // Password (line now to line+91): Depicts if the user matches the password requirements
 
-// Defining the variables 
-var myInput = document.getElementById("regPassword");
-var letter = document.getElementById("letter");
-var capital = document.getElementById("capital");
-var number = document.getElementById("number");
-var length = document.getElementById("length");
+// // Defining the variables 
+// var myInput = document.getElementById("regPassword");
+// var letter = document.getElementById("letter");
+// var capital = document.getElementById("capital");
+// var number = document.getElementById("number");
+// var length = document.getElementById("length");
 
-// TODO: Set a function that checks if the user name is unique in the storage.
+// // TODO: Set a function that checks if the user name is unique in the storage.
 
-// When the user clicks on the password field, show the message box
-myInput.onfocus = function() {
-  document.getElementById("message").style.display = "block";
-} 
+// // When the user clicks on the password field, show the message box
+// myInput.onfocus = function() {
+//   document.getElementById("message").style.display = "block";
+// } 
 
-// When the user clicks outside of the password field, hide the message box
-myInput.onblur = function() {
-  document.getElementById("message").style.display = "none";
-}
+// // When the user clicks outside of the password field, hide the message box
+// myInput.onblur = function() {
+//   document.getElementById("message").style.display = "none";
+// }
 
-// When the user starts to type something inside the password field
-myInput.onkeyup = function() {
-  // Validate lowercase letters
-  var lowerCaseLetters = /[a-z]/g;
-  if(myInput.value.match(lowerCaseLetters)) { 
-    letter.classList.remove("invalid");       
-    letter.classList.add("valid");           
-  } else {
-    letter.classList.remove("valid");
-    letter.classList.add("invalid");
-}
+// // When the user starts to type something inside the password field
+// myInput.onkeyup = function() {
+//   // Validate lowercase letters
+//   var lowerCaseLetters = /[a-z]/g;
+//   if(myInput.value.match(lowerCaseLetters)) { 
+//     letter.classList.remove("invalid");       
+//     letter.classList.add("valid");           
+//   } else {
+//     letter.classList.remove("valid");
+//     letter.classList.add("invalid");
+// }
 
-  // Validate capital letters
-  var upperCaseLetters = /[A-Z]/g;
-  if(myInput.value.match(upperCaseLetters)) { 
-    capital.classList.remove("invalid");
-    capital.classList.add("valid");
-  } else {
-    capital.classList.remove("valid");
-    capital.classList.add("invalid");
-  }
+//   // Validate capital letters
+//   var upperCaseLetters = /[A-Z]/g;
+//   if(myInput.value.match(upperCaseLetters)) { 
+//     capital.classList.remove("invalid");
+//     capital.classList.add("valid");
+//   } else {
+//     capital.classList.remove("valid");
+//     capital.classList.add("invalid");
+//   }
 
-  // Validate numbers
-  var numbers = /[0-9]/g;
-  if(myInput.value.match(numbers)) { 
-    number.classList.remove("invalid");
-    number.classList.add("valid");
-  } else {
-    number.classList.remove("valid");
-    number.classList.add("invalid");
-  }
+//   // Validate numbers
+//   var numbers = /[0-9]/g;
+//   if(myInput.value.match(numbers)) { 
+//     number.classList.remove("invalid");
+//     number.classList.add("valid");
+//   } else {
+//     number.classList.remove("valid");
+//     number.classList.add("invalid");
+//   }
 
-  // Validate length
-  if(myInput.value.length >= 8) {
-    length.classList.remove("invalid");
-    length.classList.add("valid");
-  } else {
-    length.classList.remove("valid");
-    length.classList.add("invalid");
-  }
-}
+//   // Validate length
+//   if(myInput.value.length >= 8) {
+//     length.classList.remove("invalid");
+//     length.classList.add("valid");
+//   } else {
+//     length.classList.remove("valid");
+//     length.classList.add("invalid");
+//   }
+// }
 
 // Confirm password function (line now to +10)
 
@@ -162,14 +172,14 @@ var check = function() {
 
 // if passwords don't match, the user will be stopped from creating              
 
-var match = function() {
+// var match = function() {
 
-  // if the string of the passwords don't match (indicated by !=), it will depict the alert "Passwords don't match"
-  // Otherwise it will depict the message "Creation was successfull"
-  if  (document.getElementById("regPassword").value != document.getElementById("regConfirmPassword").value) {
-  alert ("Passwords don't match");
-      } 
-    } 
+//   // if the string of the passwords don't match (indicated by !=), it will depict the alert "Passwords don't match"
+//   // Otherwise it will depict the message "Creation was successfull"
+//   if  (document.getElementById("regPassword").value != document.getElementById("regConfirmPassword").value) {
+//   alert ("Passwords don't match");
+//       } 
+//     } 
   
 
  
