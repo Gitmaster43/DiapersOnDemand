f0ecceff551bc4a8ddd3b567f405f9e57e4866b7
Anders <anva18ae@student.cbs.dk>
2018-11-25 17:31:07 +0100

    Cleaned uppp

--- js/Subscription.ChangeUserInfo.js
+++ js/Subscription.ChangeUserInfo.js
@@ -1,28 +1,33 @@
-// We are declaring a new variable containing all the registered users from the local storage
+
+//___________________________________________________________________________________________________
+// Declaring a variable containing all the registered users from the local storage
 var users = JSON.parse(localStorage.getItem("users"));
 
-// We are initiating a function called "editUser". It is triggerd by a onclick in Subscription.html
+
+//__________________________________________________________________________________________________
+// FUNCTION FOR EDITING THE LOGGED IN USER
+
+// Initiating a function called "editUser". It is triggerd by a onclick in Subscription.html
 function editUser () {
     
-    
-    
-   // We are looping through all our users
+   // Loop through all users
     for (i=0; i < users.length; i++){
         
-        // We are identifiying the loggedInUser by matching it with his unique userId
+        // Identifiying the loggedInUser by matching it with his unique userId
         // the Loop stops when it found the loggedInUser in the array with our registered users
          if(users[i].userId == localStorage.getItem('loggedInUser')){
             
             // When we have found the loggedInUser in our registered users array, we asign that user with new information
             users[i].username = document.getElementById('userName').value
-            changeUserName = document.getElementById('userName').value
             users[i].firstname = document.getElementById('firstName').value
             users[i].lastname = document.getElementById('lastName').value
             users[i].email = document.getElementById('email').value
             
+            // Only in order to change the name in the top left corner to the changed userName
+            changeUserName = document.getElementById('userName').value
         }   
    
-                
+    // Requirement for the user to type in something in the box in order to change it.            
     } if (document.getElementById('userName').value == "", 
           document.getElementById('firstName').value == "" ,
           document.getElementById('lastName').value == "",
@@ -43,7 +48,7 @@ function editUser () {
     // Triggers the function (declared in Subscription.GetUserInfo) which updates the information in the table
     displayUserInformation();
   
-    window.location = "index.html"; 
+    window.location.reload; 
 }
 
 
